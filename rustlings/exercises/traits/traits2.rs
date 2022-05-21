25d7230e98eabf5953a32157e5db446c08d0632e
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2022-05-21 17:33:55 +1200

    Tests, traits and quiz3

--- rustlings/exercises/traits/traits2.rs
+++ rustlings/exercises/traits/traits2.rs
@@ -10,22 +10,28 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
-    fn append_bar(self) -> Self;
+	fn append_bar(self) -> Self;
+}
+
+impl AppendBar for Vec<String> {
+	fn append_bar(mut self) -> Self {
+		self.push(String::from("Bar"));
+
+		self
+	}
 }
 
 //TODO: Add your code here
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn is_vec_pop_eq_bar() {
-        let mut foo = vec![String::from("Foo")].append_bar();
-        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
-        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
-    }
+	#[test]
+	fn is_vec_pop_eq_bar() {
+		let mut foo = vec![String::from("Foo")].append_bar();
+		assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+		assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+	}
 }
