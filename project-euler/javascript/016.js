1c83e52478a54ada21b19507d979d51ac0df569c
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2022-05-24 19:45:39 +1200

    Sum of digits

--- project-euler/javascript/016.js
+++ project-euler/javascript/016.js
@@ -1,2 +1,20 @@
 var assert = require("assert/strict");
 var crypto = require("crypto");
+
+function sumOfDigits(x) {
+	const bigX = BigInt(x);
+
+	return bigX
+		.toString()
+		.split("")
+		.reduce(
+			(runningTotal, currentValue) => runningTotal + +currentValue,
+			0,
+		);
+}
+
+const sum = `${sumOfDigits(Math.pow(2, 1000))}`;
+assert.strictEqual(
+	crypto.createHash("md5").update(sum).digest("hex"),
+	"6a5889bb0190d0211a991f47bb19a777",
+);
